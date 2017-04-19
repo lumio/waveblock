b84a1960030a72b63808fb5146f441d03267304d
Benjamin Falk <benjamin.falk@cyanjoe.com>
2017-04-19 22:39:58 +0200

    Wrap component to center it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 import WaveBlock from './components/WaveBlock';
 
+const AppWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const App = () => (
-  <WaveBlock />
+  <AppWrapper>
+    <WaveBlock />
+  </AppWrapper>
 );
 export default App;
