9eeac84838d441e37b6e362434dc6d0d410f1806
Benjamin Falk <benjamin.falk@cyanjoe.com>
2017-04-19 22:49:28 +0200

    Render different scales

--- src/components/WaveBlock/index.js
+++ src/components/WaveBlock/index.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 import WaveDot from '../WaveDot';
+
+const BlockWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+
+  width: 30vmin;
+  height: 30vmin;
+`;
 
 export default class WaveBlock extends React.Component {
   render() {
+    const scales = [
+      0, .1, .2,
+      .1, .2, .3,
+      .2, .3, .4,
+    ];
+
     return (
-      <div>
-        <WaveDot />
-        <WaveDot />
-        <WaveDot />
-
-        <WaveDot />
-        <WaveDot />
-        <WaveDot />
-
-        <WaveDot />
-        <WaveDot />
-        <WaveDot />
-      </div>
+      <BlockWrapper>
+        { scales.map( ( scale, key ) => (
+          <WaveDot scale={ scale } key={ key } />
+        ) ) }
+      </BlockWrapper>
     )
   }
 };
