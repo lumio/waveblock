868153e78637598b7e7ca890932f25fe98498812
Benjamin Falk <benjamin.falk@cyanjoe.com>
2017-04-19 22:48:54 +0200

    Style single dot

--- src/components/WaveDot/index.js
+++ src/components/WaveDot/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const WaveDot = () => (
-  <span>dot</span>
-)
+const DotWrapper = styled.div`
+  width: 33.33%;
+  height: 33.33%;
+  background: #000;
+
+  border-radius: 100%;
+  transform: scale( .1 );
+  transition: transform .25s ease;
+`;
+
+const WaveDot = ( props ) => (
+  <DotWrapper
+    style={ { transform: `scale( ${ props.scale ? props.scale : '.1' } )` } }
+  />
+);
+
+WaveDot.propTypes = {
+  scale: React.PropTypes.number.isRequired,
+};
 
 export default WaveDot;
