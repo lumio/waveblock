d031ede83e88799cfb15f4942dcac4325dcfda83
Benjamin Falk <benjamin.falk@cyanjoe.com>
2017-04-20 00:50:16 +0200

    Add min and max scale

--- src/components/WaveDot/index.js
+++ src/components/WaveDot/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+function stretchScale( scale, min, max ) {
+  return ( max - min ) * scale + min
+}
+
 const Dot = styled.div`
   width: 33.33%;
   height: 33.33%;
@@ -9,17 +13,26 @@ const Dot = styled.div`
 
   border-radius: 100%;
   transform: scale( .1 );
-  transition: transform .25s ease;
+  transition: transform .1s ease;
 `;
 
 const WaveDot = ( props ) => (
   <Dot
-    style={ { transform: `scale( ${ props.scale } )` } }
+    style={ { transform: `scale( ${
+      stretchScale( props.scale, props.min, props.max )
+    } )` } }
   />
 );
 
 WaveDot.propTypes = {
   scale: PropTypes.number.isRequired,
+  min: PropTypes.number,
+  max: PropTypes.number,
 };
+
+WaveDot.defaultProps = {
+  min: .08,
+  max: 3,
+}
 
 export default WaveDot;
