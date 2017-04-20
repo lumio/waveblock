c757d0110b9922e536b193ab105b3c7a82a51a39
Benjamin Falk <benjamin.falk@cyanjoe.com>
2017-04-20 21:04:22 +0200

    Fix remapData call

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ export default class App extends React.Component {
   updateData = ( data ) => {
     this.setState( {
       ...this.state,
-      data: this.remapData( data, this.state.map ),
+      data: App.remapData( data, this.state.map ),
     } );
   }
 
